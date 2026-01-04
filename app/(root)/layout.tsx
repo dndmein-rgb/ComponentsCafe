2cb7a03860c2237f511a673b23cbd905bced6c4c
Divyanshu <dnd.mein@gmail.com>
2026-01-04 13:07:03 +0530

    Initialize project with Shadcn UI and theming

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import '../globals.css';
+
+import {ViewTransitions} from 'next-view-transitions';
+import Footer from "@/components/layout/footer";
+import {Header} from "@/components/landing/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title:{
+        template:"CodeSnippet UI = Open Source Components",
+        default:"CodeSnippet UI"
+    }
+};
+
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ViewTransitions>
+      <html lang="en"  className="h-full">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased h-full flex flex-col`}
+        >
+          <Header />
+          <main className="relative w-full flex-1">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ViewTransitions>
+  );
+}
